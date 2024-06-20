9a4e96a4b00d7de9a0339b6b57863174b79e99c8
Maythas Wangcharoenwong <maythas.wangcharoenwong@gmail.com>
2024-06-20 08:48:02 +0700

    feat: Setup Cmake and basic project structure.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glfw/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+int main(int argc , char *argv[]){
+	std::cout << "Hello World";
+	return 12;
+}
