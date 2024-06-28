1c5525bb5354bdd957e390bbe457407fb8ca352e
maythas <maythas.wangcharoenwong@gmail.com>
2024-06-28 16:40:54 +0700

    style: Remove commented out code.

--- src/main.cpp
+++ src/main.cpp
@@ -42,21 +42,21 @@ int main(int argc, char *argv[])
 	// ----------------------------------------------------------------------------
 
 	Shader shader_program = Shader::Shader();
+	
 	// The dir depends on where you call it so if you call it from root, do it as if the current working directory is in root.
 	GLuint vertexShader = shader_program.compile_shader("./shader_source/light.vert.glsl", GL_VERTEX_SHADER);
 	GLuint fragmentShader = shader_program.compile_shader("./shader_source/light.frag.glsl", GL_FRAGMENT_SHADER);
 	shader_program.link_shader(vertexShader);
 	shader_program.link_shader(fragmentShader);
-
 	GLuint shader_id = shader_program.get_shader_id();
 
-	// TODO: Implement the projections to handle camera movement from here forward.
+	// Setup Callback and Camera
+	// ----------------------------------------------------------------------------
 	Camera camera = Camera();
 	Camera *p_camera = &camera;
 	CallbackManager callback_manager = CallbackManager::CallbackManager(window, p_camera);
 
-	// TODO: Remove this later when no longer needed and make one class to deal with creating Geometry, Transforming Geometry, and so on ...
-	// Simple Geometry for testing.
+	// Initialize Geometry
 	// ----------------------------------------------------------------------------
 
 	if (WIREFRAME_ON)
@@ -68,38 +68,7 @@ int main(int argc, char *argv[])
 
 	SamplePolygon sample_polygon = SamplePolygon();
 	GLuint VAO, VBO, EBO;
-	// sample_polygon.init_triangle(&VAO,&VBO,&EBO);
 	sample_polygon.init_cube(&VAO,&VBO,&EBO);
-
-	// GLuint VAOs; // VAO for each object
-	// GLuint VBOs; // Vertices, Indices, Colours
-	// GLuint EBOs; // Store Indices (How they link)
-
-	// GLfloat triangle_vert[3][3] = {
-	// 	{-0.5f, -0.5f, 0.0f},
-	// 	{0.5f, -0.5f, 0.0f},
-	// 	{0.0f, 0.5f, 0.0f}};
-
-	// GLuint triangle_inds[1][3] = {{0, 1, 2}};
-
-	// glGenVertexArrays(1, &VAOs);
-	// glGenBuffers(1, &VBOs);
-	// glGenBuffers(1, &EBOs);
-
-	// glBindVertexArray(VAOs);
-
-	// glBindBuffer(GL_ARRAY_BUFFER, VBOs);
-	// glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, &triangle_vert[0], GL_STATIC_DRAW);
-
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs);
-	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 3, &triangle_inds[0], GL_STATIC_DRAW);
-
-	// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-	// glEnableVertexAttribArray(0);
-
-	// // Unbind VAOs, VBOs, EBOs
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// glBindVertexArray(0);
 
 	// Begin Render Loop
 	// ----------------------------------------------------------------------------
@@ -113,11 +82,9 @@ int main(int argc, char *argv[])
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader_id);
 		glBindVertexArray(VAO);
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-		glDrawElements(GL_POINT, 36, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 12*3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_POINT, 12*3, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
