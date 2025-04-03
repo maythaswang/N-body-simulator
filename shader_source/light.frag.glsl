cbeff25fd1f776f7446647b2839cd72812fb0681
Maythas <maythas.wangcharoenwong@gmail.com>
2025-04-03 16:23:05 +0700

    feat: Implement skeleton for color-mass fs.

--- shader_source/light.frag.glsl
+++ shader_source/light.frag.glsl
@@ -1,5 +1,20 @@
 #version 430 core
+
+in float star_mass;
+
 out vec4 FragColor;
+
+// FIX THIS LATER
+vec4 get_color(){
+    vec4 star_color = vec4(1.0f, 0.88f, 0.49f, 0.7f);
+    // if(star_mass > 10000){ 
+    //     star_color = vec4(1.0f, 0.7f, 0.7f, 0.7f);
+    // }
+
+    return star_color;
+}
+
 void main(){
-    FragColor = vec4(1.0f, 0.88f, 0.49f, 0.7f);
+    // FragColor = vec4(1.0f, 0.88f, 0.49f, 0.7f);
+    FragColor = get_color();
 }
--- shader_source/light.vert.glsl
+++ shader_source/light.vert.glsl
@@ -10,13 +10,15 @@ layout (std430, binding = 0) buffer particle_position {
 	vec4 position[]; 
 };
 
-// layout (std430, binding = 3) buffer particle_mass { 
-// 	vec4<float> mass[]; 
-// };
+layout (std430, binding = 3) buffer particle_mass { 
+	float mass[]; 
+};
 
 // Uniform Variables
 uniform mat4 modelview;
 uniform mat4 projection;
+
+out float star_mass;
 
 void main() {
 //   gl_PointSize = 10.0f;
@@ -24,4 +26,5 @@ void main() {
   vec4 new_pos = position[gl_VertexID];
   new_pos.w=1;
 	gl_Position = projection * modelview * new_pos;
+  star_mass = mass[gl_VertexID];
 }
