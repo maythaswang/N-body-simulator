704fdbf1aa8eadf5dbfd295f25d54e62d537f75a
Maythas <maythas.wangcharoenwong@gmail.com>
2025-04-03 15:29:22 +0700

    feat: Vertex shader now draws from SSBO.

--- shader_source/light.vert.glsl
+++ shader_source/light.vert.glsl
@@ -6,9 +6,9 @@ layout(location = 0) in vec3 v_pos;
 
 
 // Position of item
-// layout (std430, binding = 0) buffer particle_position { 
-// 	vec4<float> position[]; 
-// };
+layout (std430, binding = 0) buffer particle_position { 
+	vec4 position[]; 
+};
 
 // layout (std430, binding = 3) buffer particle_mass { 
 // 	vec4<float> mass[]; 
@@ -20,6 +20,8 @@ uniform mat4 projection;
 
 void main() {
 //   gl_PointSize = 10.0f;
-  gl_Position = projection * modelview * vec4(v_pos, 1.0);
-	// gl_Position = projection * modelview * position[gl_VertexID];
+  // gl_Position = projection * modelview * vec4(v_pos, 1.0);
+  vec4 new_pos = position[gl_VertexID];
+  new_pos.w=1;
+	gl_Position = projection * modelview * new_pos;
 }
