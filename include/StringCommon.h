aa0c50da914b9fd454e69aaceaf63ac48fe15ba8
Maythas <maythas.wangcharoenwong@gmail.com>
2025-04-10 12:56:57 +0700

    chore: update help message.

--- include/StringCommon.h
+++ include/StringCommon.h
@@ -5,7 +5,12 @@ inline constexpr char g_welcome_message[] =
     "+------------------------------------------------+\n";
 
 inline constexpr char g_controls_help[] =
-    "Enable GUI mode for some extra settings! (press N to toggle)\n\n"
+    "[HELP MENU]\n"
+    "Enable GUI mode for some extra settings! (press N to toggle)\n"
+    "   - Bloom intensity setting\n"
+    "   - Gamma / Exposure settings\n"
+    "   - Setup information page\n"
+    "\n"
     "Window controls:\n"
     "   esc                             : exit simulation.\n"
     "   h                               : display controls on the console.\n"
@@ -18,7 +23,9 @@ inline constexpr char g_controls_help[] =
     "   m                               : toggle mass-size.\n"
     "   c                               : toggle mass-color.\n"
     "   n                               : toggle GUI.\n"
-    "\nCamera controls:\n"
+    "\nps. mass-size and wireframe mode only shows when instancing is on\n"
+    "\n"
+    "Camera controls:\n"
     "   middle mouse drag + left shift  : pan the camera in the mouse drag direction.\n"
     "   middle mouse drag + left ctrl   : zoom in/out by dragging mouse forward and vice versa. (free-flying mode: move forward / backwards.)\n"
     "   middle mouse drag               : orbit the camera about the center in the mouse drag direction. (free-flying mode: rotate the center.)\n"
